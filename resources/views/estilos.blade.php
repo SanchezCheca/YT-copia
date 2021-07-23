19eed2fab5aeae9ec9e7643674062c72499ba472
Daniel Sánchez <danielsanchezc6@gmail.com>
2021-07-23 19:28:37 +0200

    Cambio menor

--- resources/views/estilos.blade.php
+++ resources/views/estilos.blade.php
@@ -17,3 +17,13 @@ if (sizeof($rutaExploded) > 1) { ?>
 ?>
 <link rel="stylesheet" href="{{url('fonts/all.css')}}">
 <link rel="stylesheet" href="{{url('css/estilos.css')}}">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-67Q4GG8WL9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-67Q4GG8WL9');
+</script>
